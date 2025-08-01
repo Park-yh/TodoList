e467772d090c67306c0e01b0fc4c3dfdc72925a5
Park-yh <pyh5421@naver.com>
2025-08-01 12:16:45 +0900

    Request, Response 생성

--- src/main/java/com/example/todolist/dto/TodoListRequest.java
+++ src/main/java/com/example/todolist/dto/TodoListRequest.java
@@ -1,4 +1,11 @@
 package com.example.todolist.dto;
 
+import lombok.Getter;
+
+@Getter
 public class TodoListRequest {
+    private String title;
+    private String content;
+    private String author;
+    private String password;
 }
